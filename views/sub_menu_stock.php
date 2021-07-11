2a6a8114b65a1f82cfa84c11cd617653902c124f
fkamau <fkamau@dewcis.com>
2021-07-11 17:06:58 +0300

    updated header and stock master

--- views/sub_menu_stock.php
+++ views/sub_menu_stock.php
@@ -22,7 +22,7 @@
                             <span class="icon mr-3"><i class="fa fa-database"></i></span>Stock Master
                         </a>
                     </li>
-                    <ul class="collapse list-unstyled list-group-item list-group-item-action " id="homeSubmenu" data-parent="#accordion">
+                    <ul class="list-unstyled list-group-item list-group-item-action " id="homeSubmenu" data-parent="#accordion">
                         <li>
                             <a href="./stock_index.php?view=stock-master" class="list-group-item list-group-item-action d-flex align-items-center">
                                 <span class="icon mr-3"><i class="fa fa-database"></i></span>Stock Grid

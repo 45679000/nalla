6552bac33d26969b1d225b473976e722c164b591
fkamau <fkamau@dewcis.com>
2021-07-12 16:17:30 +0300

    updated

---
+++ reports/blend_sheet.php
@@ -0,0 +1,20 @@
+<?php 
+ ob_start();
+
+    $path_to_root = "../";
+    $path_to_root1 = "../../";
+    include_once($path_to_root1."phpjasperxml/PHPJasperXML.inc.php");
+    include_once ($path_to_root.'setting.php');
+    
+    $PHPJasperXML = new PHPJasperXML();
+    $PHPJasperXML->debugsql = false;
+    if(isset($_GET['blendno'])){
+        $PHPJasperXML->arrayParameter=array("blend_no"=>$_GET['blendno']);
+        $PHPJasperXML->load_xml_file("jrxmlFiles/blend_sheet.jrxml");
+        $PHPJasperXML->transferDBtoArray($server,$user,$pass,$db);
+        ob_end_clean();
+        $PHPJasperXML->outpage("I");
+    }
+?>
+
+       

2b644e3f91d65170d66a1836a668489ccdd5a545
fkamau <fkamau@dewcis.com>
2021-07-17 15:21:36 +0300

    added boostrap to login page

--- assets/js/warehousing.js
+++ assets/js/warehousing.js
@@ -13,3 +13,18 @@ function loadUnclosedBlends() {
         }
     });
 }
+function dashboard(){
+    $.ajax({
+        type: "POST",
+        dataType: "html",
+        data: {
+            action: "dashboard"
+        },
+        cache: true,
+        url: "../modules/blending/blend_action.php",
+        success: function (data) {
+            $('#tableData').html(data);
+
+        }
+    });
+}

332080b1d104d035cfbd589a91a0d0565bff93d2
fkamau <fkamau@dewcis.com>
2021-07-17 12:19:08 +0300

    updated blend sheet

--- views/login.php
+++ views/login.php
@@ -42,9 +42,9 @@ session_start();
 
 ?>
 <html >
-    <head  >
+    <head>
     <link rel="stylesheet" href="<?php echo $path_to_root ?>assets/css/login.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo $path_to_root ?>assets/css/boostrap.min.css">
   </head>
 <body class="bg">
 <div class="container register">

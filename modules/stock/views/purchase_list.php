ed90406ea6fe509e054cbd7ad1f7af209eeb872a
root <fchege22@gmail.com>
2021-09-02 16:06:48 +0300

    updated

--- modules/stock/views/purchase_list.php
+++ modules/stock/views/purchase_list.php
@@ -1,10 +1,82 @@
+<style>
+.navbuttons {
+    display: inline-block;
+    overflow: auto;
+    overflow-y: hidden;
+
+    max-width: 100%;
+    margin: 0 0 1em;
+    height: 50px;
+
+    white-space: nowrap;
+
+}
+
+.navbuttons LI {
+    display: inline-block;
+    vertical-align: top;
+    padding: 10px;
+    
+}
+
+</style>
+
 <div class="col-md-12 col-lg-12">
     <div class="card">
         <div class="card-header text-center">
             <div id="notificationId">
             </div>
-            <div class="card">
-                <form method="post">
+            <!-- <div class="card">
+                <div class="card">
+                <div class="nav">
+                    <ul class="navbuttons">
+                        <li class="fa fa-file text-success">
+                            <a class="plist" id="2021-28">
+                                <span>CSL  2021-28</span>  
+                            </a>
+                        </li>
+                        <li class="fa fa-file text-success">
+                            <a class="plist" id="2021-29">
+                                <span>CSL 2021-29</span>  
+                                
+                            </a> 
+                        </li>   
+                        <li class="fa fa-file text-success">
+                            <a class="plist" id="2021-30">
+                                <span>CSL 2021-30</span>  
+                                
+                            </a>  
+                        </li>   
+                        <li class="fa fa-file text-success">
+                            <a class="plist" id="2021-31">
+                            <span>CSL 2021-31</span>  
+                            </a>
+                        </li>   
+                        <li class="fa fa-file text-success">
+                            <a class="plist" id="2021-32">
+                            <span>CSL 2021-32</span>  
+                            </a>
+                        </li>   
+                        <li class="fa fa-file text-success">
+                            <a class="plist" id="2021-33">
+                            <span>CSL 2021-33</span>  
+                            </a>
+                        </li>  
+                         <li class="fa fa-file text-success">
+                            <a class="plist" id="2021-34">
+                            <span>CSL 2021-34</span>  
+                            </a>
+                        </li>   
+                        <li class="fa fa-file text-success">
+                            <a class="plist" id="2021-35">
+                            <span>CSL 2021-35</span>  
+                            </a>
+                        </li>  
+                        
+                    </ul>
+                </div>
+                </div> -->
+                <!-- <form method="post">
                     <div class="row justify-content-center">
                         <div class="col-md-6 well">
                             <div class="form-group form-inline">
@@ -15,11 +87,60 @@
                             </div>
                         </div>
                     </div>
-                </form>
-            </div>
+                </form> -->
+            <!-- </div> -->
         </div>
         <div class="card-body p-6">
             <div class="expanel expanel-secondary">
+                <div class="card-header">
+                <div class="nav">
+                    <ul class="navbuttons">
+                        <li class="fa fa-file text-success">
+                            <a class="plist" id="2021-28">
+                                <span>CSL  2021-28</span>  
+                            </a>
+                        </li>
+                        <li class="fa fa-file text-success">
+                            <a class="plist" id="2021-29">
+                                <span>CSL 2021-29</span>  
+                                
+                            </a> 
+                        </li>   
+                        <li class="fa fa-file text-success">
+                            <a class="plist" id="2021-30">
+                                <span>CSL 2021-30</span>  
+                                
+                            </a>  
+                        </li>   
+                        <li class="fa fa-file text-success">
+                            <a class="plist" id="2021-31">
+                            <span>CSL 2021-31</span>  
+                            </a>
+                        </li>   
+                        <li class="fa fa-file text-success">
+                            <a class="plist" id="2021-32">
+                            <span>CSL 2021-32</span>  
+                            </a>
+                        </li>   
+                        <li class="fa fa-file text-success">
+                            <a class="plist" id="2021-33">
+                            <span>CSL 2021-33</span>  
+                            </a>
+                        </li>  
+                         <li class="fa fa-file text-success">
+                            <a class="plist" id="2021-34">
+                            <span>CSL 2021-34</span>  
+                            </a>
+                        </li>   
+                        <li class="fa fa-file text-success">
+                            <a class="plist" id="2021-35">
+                            <span>CSL 2021-35</span>  
+                            </a>
+                        </li>  
+                        
+                    </ul>
+                </div>
+                </div>
                 <div class="card">
                     <div class="card-header">
                         <div id="purchaseListactions" class="card-options">
@@ -56,12 +177,28 @@
     <script src="../../assets/plugins/select2/select2.full.min.js"></script>
     <script>
     $(function() {
+        var saleno = '2021-35';
+        loadPlist(saleno)
+
+        var formData = {
+                saleno: saleno,
+                action: "confirmed-purchase-list"
+            
+            };
+        $(".plist").click(function(element){
+            var click = localStorage.getItem("click");
+
+            var saleno = $(this).attr("id");
+            loadPlist(saleno);
+        })
+
         $('select').on('change', function() {
             var saleno = $('#saleno').find(":selected").text();
             localStorage.setItem("saleno", saleno);
             var formData = {
                 saleno: saleno,
                 action: "confirmed-purchase-list"
+            
             };
             $.ajax({
                 type: "POST",
@@ -97,6 +234,13 @@
                                     titleAttr: 'PDF'
                                 }
                             ],
+                            "columnDefs": [
+                                {
+                                    "targets": [ 20 ],
+                                    "visible": false,
+                                    "searchable": false
+                                }
+                            ]
                             // "scrollCollapse": true,
                         });
                         table.buttons().containers().appendTo(
@@ -107,5 +251,62 @@
 
             });
         });
+
+        function loadPlist(saleno){
+            $.ajax({
+                type: "POST",
+                dataType: "html",
+                url: "../../modules/finance/finance_action.php",
+                data: {
+                    action:"confirmed-purchase-list",
+                    saleno:saleno
+                },
+                success: function(data) {
+                    $('#purchaseList').html(data);
+                    $(document).ready(function() {
+                        var table = $('#purchaseListTable').DataTable({
+                            lengthChange: false,
+                            select: true,
+                            "pageLength": 100,
+                            dom: 'Bfrtip',
+                            buttons: [{
+                                    extend: 'copyHtml5',
+                                    text: 'COPY<i class="fa fa-clipboard"></i>',
+                                    titleAttr: 'Copy Paste'
+                                },
+                                {
+                                    extend: 'excelHtml5',
+                                    text: 'EXCEL <i class="fa fa-file-excel-o"></i>',
+                                    titleAttr: 'Excel'
+                                },
+                                {
+                                    extend: 'csvHtml5',
+                                    text: 'CSV <i class="fa fa-file-text"></i>',
+                                    titleAttr: 'CSV'
+                                },
+                                {
+                                    extend: 'pdfHtml5',
+                                    text: 'PDF <i class="fa fa-file-pdf-o"></i>',
+                                    titleAttr: 'PDF'
+                                }
+                            ],
+                            "columnDefs": [
+                                {
+                                    "targets": [ 20 ],
+                                    "visible": false,
+                                    "searchable": false
+                                }
+                            ]
+                            // "scrollCollapse": true,
+                        });
+                        if(click> 0){
+                            table.buttons().containers().appendTo('#purchaseListactions');
+                        }
+
+                    });
+                },
+
+            });
+        }
     });
     </script>

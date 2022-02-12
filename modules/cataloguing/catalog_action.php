7f1aa907e159f1b1edf8d5b549e13a2d433c44b1
arapkorir <kkip762@gmail.com>
2022-02-12 15:34:59 +0000

    Update catalog_action.php

--- modules/cataloguing/catalog_action.php
+++ modules/cataloguing/catalog_action.php
@@ -111,9 +111,6 @@
 		$saleno = isset($_POST['saleno']) ? $_POST['saleno'] : '';
 		$broker = isset($_POST['broker']) ? $_POST['broker'] : '';
 		$category = isset($_POST['category']) ? $_POST['category'] : '';
-		// $saleno = '2022-01';
-		// $broker = 'ALS';
-		// $category = 'leaf';
 		$imports = $CatalogController->closingCatalogue($saleno, $broker,$category);
 		$html = "";
 		if(sizeOf($imports)>0){

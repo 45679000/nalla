e8178e1e769d46f27da95fd0919f2aca3b216b92
kkip762@gmail.com <kkip762@gmail.com>
2022-04-27 11:40:55 +0300

    stock -> Allocate stock

--- controllers/StockController.php
+++ controllers/StockController.php
@@ -362,7 +362,7 @@
             LEFT JOIN 0_debtors_master ON closing_stock.client_id = 0_debtors_master.debtor_no
             LEFT JOIN (SELECT mark, country FROM mark_country GROUP BY mark) AS a ON a.mark = closing_stock.mark 
             LEFT JOIN (SELECT code, id FROM grading_comments GROUP BY code) AS b ON b.code = closing_stock.comment 
-            LEFT JOIN shippments ON shippments.stock_id = closing_stock.stock_id WHERE shippments.id IS NULL LIMIT 20";
+            LEFT JOIN shippments ON shippments.stock_id = closing_stock.stock_id WHERE shippments.id IS NULL ORDER BY line_no DESC";
             return $this->executeQuery();
         }
         public function sumTotal($columnname, $tablename){
